1cb2a612465ee484d1399a3ef04c2e1e9858618f
mredgar2004 <edgarzombie80@gmail.com>
2025-07-24 23:44:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'MR....EDGAR-XMD',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  botName: 'MR....EDGAR-XMD',
+  botOwner:'MR....EDGAR-XMD', // Your name
+  ownerNumber: '255625501821', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey:'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
